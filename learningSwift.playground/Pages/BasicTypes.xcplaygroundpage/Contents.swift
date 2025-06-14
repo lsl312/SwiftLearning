3a5d2ed0a299cbd16a86e020003720ab97f80d40
lsl312 <lsl312@163.com>
2025-06-14 18:07:40 +0800

    Initial Commit

---
+++ learningSwift.playground/Pages/BasicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+//String
+let myFirstItem: String = "Hello, world!"
+let myTitle = myFirstItem
+
+//Boolean
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+//Swift is a type-safe language
+//let mySecondItem: Bool = "Hello, world!" [Wrong!!!]
+
+//Date
+let myFirstDate: Date = Date()
+
+//Int
+let myFirstNumber: Int = 1
+
+//Use Double for math
+let SecondNumber: Double = 1.0
+
+//Use CGFloat for UI
+let myThirdNumber: CGFloat = 1.0
---
+++ learningSwift.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+// this is a single-line comment
+
+/*
+ This is a multi-line comment
+ ohhhhh
+ hahahaha
+ */
+
+// Naming Conventions
+
+let firstGreeting = "Hello, world!"
